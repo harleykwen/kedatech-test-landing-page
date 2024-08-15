eade1d8c0ad075d949aa60040ffb200bf1a0c339
harleykwen <harleykwen.work@gmail.com>
2024-08-16 01:39:45 +0700

    slicing footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import { Flex } from '@chakra-ui/react'
+import { Container, Flex, Text } from '@chakra-ui/react'
 
 const Footer: React.FC = () => {
     return (
-        <Flex padding='1rem'>
-            Footer
+        <Flex backgroundColor='#252430'>
+            <Container 
+                paddingTop='20px' 
+                paddingBottom='60px' 
+                maxWidth='container.xl'
+            >
+                <Flex alignItems='center' justifyContent='space-between'>
+                    <Text
+                        color='white'
+                        fontWeight='300'
+                        fontSize='14px'
+                    >© 2024 KeDa Tech. All rights reserved.</Text>
+                    <Text
+                        color='white'
+                        fontWeight='300'
+                        fontSize='14px'
+                    >Terms & Conditions ∙ Privacy Policy</Text>
+                </Flex>
+            </Container>
         </Flex>
     )
 }
